7866b4c368339352cf9a65c0eed3a89cb94f39f2
hanifiamdev <hanifamrullahdev@gmail.com>
2022-01-22 05:14:14 +0700

    ListView - Kondisi overflow karena tidak menggunakan listview

---
+++ listview/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Widget> widgets = [];
+
+  _MyAppState() {
+    for (int i = 0; i < 15; i++) {
+      widgets.add(Text(
+        "Data ke-" + i.toString(),
+        style: TextStyle(fontSize: 70),
+      ));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("Latihan ListView"),
+      ),
+      body: Column(
+        children: widgets,
+      ),
+    ));
+  }
+}
