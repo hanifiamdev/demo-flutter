d4a0ddeef4e2152c7aa884d3a9bb8ea9ea6090d2
hanifiamdev <hanifamrullahdev@gmail.com>
2022-01-21 17:07:53 +0700

    Projet Learn Flutter

---
+++ aplikasi_hello_world/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Aplikasi Hello World"),
+        ),
+        body: Center(
+            child: Container(
+                color: Colors.lightBlue,
+                width: 150,
+                height: 50,
+                child: Text(
+                  "Saya sedang melatih kemampuan flutter saya",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w700),
+                ))),
+      ),
+    );
+  }
+}
