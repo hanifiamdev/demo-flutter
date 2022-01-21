46143fe9ebc69bbd8fe84809b0a9d6b4bcae2a82
hanifiamdev <hanifamrullahdev@gmail.com>
2022-01-22 06:44:50 +0700

    TextStyle - Add Font External Source

---
+++ text_style/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Latihan Text Style"),
+          ),
+          body: Center(
+            child: Text(
+              "Ini adalah Text",
+              style: TextStyle(
+                  fontFamily: "fontExternal",
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30,
+                  decoration: TextDecoration.underline,
+                  decorationColor: Colors.red,
+                  decorationThickness: 2,
+                  decorationStyle: TextDecorationStyle.wavy),
+            ),
+          )),
+    );
+  }
+}
