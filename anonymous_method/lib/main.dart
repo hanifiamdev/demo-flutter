beeea4bea5303e886e6b569720d6a2fd5f7d975c
hanifiamdev <hanifamrullahdev@gmail.com>
2022-01-22 04:10:07 +0700

    Anonymous Method - Prepare Simulasi anonymous method

---
+++ anonymous_method/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Anonymous Method"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Ini adalah Text"),
+              RaisedButton(child: Text("Tekan saya"), onPressed: null)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
