8615a289016e1a423597f00bb315d0241b57ac94
hanifiamdev <hanifamrullahdev@gmail.com>
2022-01-22 21:00:55 +0700

    Draggable, DragTarget, Sizebox, Material - error untuk DragTarget

--- draggable/lib/main.dart
+++ draggable/lib/main.dart
@@ -10,6 +10,9 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   Color color1 = Colors.red;
   Color color2 = Colors.amber;
+  Color targetColor = Colors.green;
+  bool isAccepted = false;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -41,10 +44,8 @@ class _MyAppState extends State<MyApp> {
                       height: 50,
                       child: Material(
                         color: Colors.grey,
-                        shape:
-                            StadiumBorder(), // ini membuat pojokan jadi melengkung , karena width dan height kebetulan sama 50 maka membentuk serperti lingkaran
-                        elevation: // elevation untuk mengatur ketinggian sehingga memunculkan efek shadow
-                            0,
+                        shape: StadiumBorder(),
+                        elevation: 0,
                       ),
                     ),
                     //ketika kodisi sedang di drag
@@ -53,14 +54,38 @@ class _MyAppState extends State<MyApp> {
                       height: 50,
                       child: Material(
                         color: color1.withOpacity(0.7),
-                        shape:
-                            StadiumBorder(), // ini membuat pojokan jadi melengkung , karena width dan height kebetulan sama 50 maka membentuk serperti lingkaran
-                        elevation: // elevation untuk mengatur ketinggian sehingga memunculkan efek shadow
-                            3,
+                        shape: StadiumBorder(),
+                        elevation: 3,
                       ),
                     ),
                   ),
                 ],
+              ),
+              DragTarget<Color>(
+                onWillAccept: (value) => true, // menerima draggable
+                onAccept: (value) {
+                  isAccepted = true;
+                  targetColor = value;
+                }, // fungsi yang akan dilakukan ketika menerima dragable
+                builder: (context, candidates, rejected) {
+                  return (isAccepted)
+                      ? SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: Material(
+                            color: targetColor,
+                            shape: StadiumBorder(),
+                          ),
+                        )
+                      : SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: Material(
+                            color: Colors.black26,
+                            shape: StadiumBorder(),
+                          ),
+                        );
+                }, // untuk membuat widget dari dragtarget ini
               )
             ],
           )),
